2caf415c5292369baaaa006d25aae8fcf5e61dbf
Gonçalo Morais <goncalo@bridge-u.com>
2019-08-26 11:08:51 +0100

    Consider missing session

--- store/index.js
+++ store/index.js
@@ -1,6 +1,7 @@
 export const actions = {
   nuxtServerInit({ commit }, { req }) {
     if (!req) return
+    if (!req.session) return
     if (!req.session.grant) return
 
     const accessToken = req.session.grant.response.access_token
